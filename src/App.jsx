c664cd1a7d0645ee95a789bb6157baa7860cff58
flaviovidalhuax <flaviovidalhuax@gmail.com>
2022-10-03 18:45:42 -0500

    bug2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import getRandomNumber from './utils/getRandomNumber'
 import axios from 'axios'
--- src/components/CardResident.jsx
+++ src/components/CardResident.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import './styles/cardResident.css'
+import './styles/cardResident'
 const CardResident = ({url}) => {
     
         const [ressident, setRessident] = useState()
