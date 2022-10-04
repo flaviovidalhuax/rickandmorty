f58526e2560b85bcfe8f313e974f13f2c1c58a2d
flaviovidalhuax <flaviovidalhuax@gmail.com>
2022-10-03 22:27:04 -0500

    bug4

--- src/components/CardResident.jsx
+++ src/components/CardResident.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import './styles/cardResident'
+import './styles/cardResident.css'
 const CardResident = ({url}) => {
     
         const [ressident, setRessident] = useState()
